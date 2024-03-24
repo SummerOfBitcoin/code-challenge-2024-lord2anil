14b5553ec3fc609858859ca35d907ce07f28cfe7
lord2anil <a8208226@gmail.com>
2024-03-24 12:47:44 +0530

    done assemble block

--- rustBlock/src/main.rs
+++ rustBlock/src/main.rs
@@ -4,12 +4,16 @@ use std::fs::File;
 use std::io::Read;
 use serde_json;
 mod validate_transactions;
+mod assemble_block;
+mod coinbase_transaction;
 use validate_transactions::validate_transactions;
-
-
+use assemble_block::*;
+use coinbase_transaction::*;
 mod transacton_struct;
 
 use transacton_struct::Transaction;
+mod mine_block;
+use mine_block::*;
 
 fn main() {
     let folder_path= "../mempool";
@@ -34,6 +38,19 @@ fn main() {
     }
     transactions= validate_transactions(&transactions).clone();
     println!("{:?} {x}", transactions.len());
+    let coinbase_transaction: Transaction = construct_coinbase_transaction(1000000, 50000); // Example block reward and transaction fees
+    let block = assemble_block(transactions, coinbase_transaction);
+
+    // Print the assembled block
+    // println!("{:#?}", block);
+    let difficulty_target = "0000ffff00000000000000000000000000000000000000000000000000000000";
+
+    // Mine the block
+    let mined_block = mine_block(block, difficulty_target);
+
+    // Print the mined block
+    println!("{:#?}", mined_block);
+}
 
    
-}
+
