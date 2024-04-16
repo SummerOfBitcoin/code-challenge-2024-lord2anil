9f0fb35953171891aa78ea6784597a6170b1741d
lord2anil <a8208226@gmail.com>
2024-04-16 15:33:07 +0530

    Update coinbase transaction construction in coinbase_transaction.rs

--- rustBlock/src/coinbase_transaction.rs
+++ rustBlock/src/coinbase_transaction.rs
@@ -12,7 +12,7 @@ pub fn construct_coinbase_transaction(
     let mut coinbase_inputs = Vec::new();
     let coinbase_input = TransactionInput {
         txid: String::from("0000000000000000000000000000000000000000000000000000000000000000"),
-        vout: 0, // Coinbase transaction does not reference any previous output
+        vout: 4294967295, // Coinbase transaction does not reference any previous output
         prevout: PrevOut {
             scriptpubkey: String::from(""), // Arbitrary data for the coinbase input
             scriptpubkey_asm: String::from(""),
@@ -20,7 +20,7 @@ pub fn construct_coinbase_transaction(
             scriptpubkey_address: String::from(""),
             value: 0, // Value is typically set to 0 for coinbase inputs
         },
-        scriptsig: String::from(""), // Empty script signature for coinbase input
+        scriptsig: String::from("03233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100"), // Empty script signature for coinbase input
         scriptsig_asm: String::from(""),
         witness: Vec::new(),
         is_coinbase: true,
@@ -32,7 +32,7 @@ pub fn construct_coinbase_transaction(
 
     let mut coinbase_outputs = Vec::new();
     let miner_output = TransactionOutput {
-        scriptpubkey:String::from(""), // Use the generated script 
+        scriptpubkey:String::from("41047eda6bd04fb27cab6e7c28c99b94977f073e912f25d1ff7165d9c95cd9bbe6da7e7ad7f2acb09e0ced91705f7616af53bee51a238b7dc527f2be0aa60469d140ac"), // Use the generated script 
         scriptpubkey_asm: String::from(""), // You would need to fill this if required
         scriptpubkey_type: String::from("p2pkh"),
         scriptpubkey_address: String::from(""), // Derive if needed
