c771ac120e958bb50b797938ac33ec0e1be091ac
lord2anil <a8208226@gmail.com>
2024-04-18 12:04:30 +0530

    refactor some files

--- rustblock/src/validate_transactions.rs
+++ rustblock/src/validate_transactions.rs
@@ -7,9 +7,11 @@ use sha2::{Digest, Sha256};
 
 use crate::Transaction;
 
-use super::validations_utiles::*;
+
 
 use super::utiles::{convert_to_4bytes, convert_to_8bytes, int_to_varint};
+
+use super::validation_scripts::p2pkh::p2pkh_validate;
 
 
 pub fn validate_transactions(transactions: &[Transaction]) -> Vec<Transaction> {
@@ -30,32 +32,7 @@ pub fn validate_transactions(transactions: &[Transaction]) -> Vec<Transaction> {
 
 
 
-fn p2pkh_validate(t: &Transaction, idx: usize) -> bool {
-    let scriptsig_asm1 = t.vin[idx].scriptsig_asm.clone();
-    let binding = scriptsig_asm1.split(" ").collect::<Vec<&str>>();
-    let pub_key = binding.last().unwrap();
-    let pub_key = hex::decode(pub_key).unwrap();
-    let pub_key_hash256 = Sha256::digest(pub_key);
-    let pub_key_ripemd160 = Ripemd160::digest(&pub_key_hash256);
-    let pub_key_ripemd160_hex = hex::encode(pub_key_ripemd160);
-    let pub_key_hash = t.vin[idx]
-        .prevout
-        .scriptpubkey_asm
-        .split(" ")
-        .collect::<Vec<&str>>()[3];
-    // println!("{}   {}", pub_key_ripemd160_hex, pub_key_hash);
-    if pub_key_ripemd160_hex != pub_key_hash {
-       
-        return false;
-    }
-    if !p2pkh_verify_signature(t.clone(), idx) {
-       
-        return false;
-    }
 
-    
-    true
-}
 
 
 
--- rustblock/src/validation_scripts/p2pkh.rs
+++ rustblock/src/validation_scripts/p2pkh.rs
@@ -8,9 +8,35 @@ use crate::Transaction;
 
 
 
-use super::utiles::{convert_to_4bytes, convert_to_8bytes, int_to_varint};
+use crate::utiles::{convert_to_4bytes, convert_to_8bytes, int_to_varint};
 
 
+pub fn p2pkh_validate(t: &Transaction, idx: usize) -> bool {
+    let scriptsig_asm1 = t.vin[idx].scriptsig_asm.clone();
+    let binding = scriptsig_asm1.split(" ").collect::<Vec<&str>>();
+    let pub_key = binding.last().unwrap();
+    let pub_key = hex::decode(pub_key).unwrap();
+    let pub_key_hash256 = Sha256::digest(pub_key);
+    let pub_key_ripemd160 = Ripemd160::digest(&pub_key_hash256);
+    let pub_key_ripemd160_hex = hex::encode(pub_key_ripemd160);
+    let pub_key_hash = t.vin[idx]
+        .prevout
+        .scriptpubkey_asm
+        .split(" ")
+        .collect::<Vec<&str>>()[3];
+    // println!("{}   {}", pub_key_ripemd160_hex, pub_key_hash);
+    if pub_key_ripemd160_hex != pub_key_hash {
+       
+        return false;
+    }
+    if !p2pkh_verify_signature(t.clone(), idx) {
+       
+        return false;
+    }
+
+    
+    true
+}
 pub fn p2pkh_verify_signature(t: Transaction, idx: usize) -> bool {
     let mut transaction_data = String::new();
     // 4 bits version, in little endian
