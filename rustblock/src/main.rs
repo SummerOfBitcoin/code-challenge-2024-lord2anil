fdb4bcbf07adc4b21961372079b0fd6012d42fe5
lord2anil <a8208226@gmail.com>
2024-04-23 23:40:31 +0530

    limit 2800

--- rustblock/src/main.rs
+++ rustblock/src/main.rs
@@ -60,7 +60,7 @@ fn main() {
     let mut cnt=0;
      for x in transactions.iter(){
         cnt+=1;
-        if cnt<=2600{
+        if cnt<=2800{
             needed_transactions.push(x.clone());
         }else{
             extra_transactions.push(x.clone());
