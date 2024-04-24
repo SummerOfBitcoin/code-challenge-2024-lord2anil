6f2890ea14cfef20e0a7d12e95ca4aeb1a943897
lord2anil <a8208226@gmail.com>
2024-04-24 09:48:44 +0530

    33 limit

--- rustblock/src/validate_transactions.rs
+++ rustblock/src/validate_transactions.rs
@@ -9,7 +9,7 @@ pub fn validate_transactions(transactions: &[Transaction]) -> Vec<Transaction> {
         if is_valid_transaction(transaction) {
             valid_transactions.push(transaction.clone());
         }
-        if valid_transactions.len() == 3502 {
+        if valid_transactions.len() == 3300 {
             break;
         }
        
